f991696d8011e63f247d1e176cc1879040c8d6b6
Swapnali <swapnalishilimkar23@gmail.com>
2023-09-16 22:40:53 +0530

    new class added in main branch

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -4,5 +4,14 @@
     {
         Console.WriteLine("Welcome");
         Console.WriteLine("code added in branch1");
+        sample sample = new sample();
+        sample.display();
+    }
+}
+class sample
+{
+    public void display()
+    {
+        Console.WriteLine("code added in main branch");
     }
 }
